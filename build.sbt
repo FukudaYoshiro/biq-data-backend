8d10f84a82b9ccdda3d961fa1c194a7357e6fa81
FukudaYoshiro <elsa55queen@gmail.com>
2016-11-29 22:35:10 +0900

    stream done

--- build.sbt
+++ build.sbt
@@ -9,8 +9,12 @@ scalaVersion := "2.11.2"
 crossScalaVersions := Seq("2.10.4", "2.11.2")
 
 libraryDependencies ++= Seq(
-  "org.scalatest" %% "scalatest" % "2.2.1" % "test",
-  "org.scalacheck" %% "scalacheck" % "1.11.5" % "test"
+    "org.twitter4j" % "twitter4j-core" % "4.0.4",
+    "org.twitter4j" % "twitter4j-async" % "4.0.4",
+    "org.twitter4j" % "twitter4j-stream" % "4.0.4",
+    "org.twitter4j" % "twitter4j-media-supsport" % "4.0.4",
+    "org.scalatest" %% "scalatest" % "2.2.1" % "test",
+    "org.scalacheck" %% "scalacheck" % "1.11.5" % "test"
 )
 
 initialCommands := "import example._"
--- src/main/scala/App.scala
+++ src/main/scala/App.scala
@@ -1,7 +1,76 @@
 package example
 
+import twitter4j.auth.AccessToken
+import twitter4j.{GeoLocation, Status, StatusListener, _}
+import scala.collection.mutable.ArrayBuffer
+
 object App {
-  def main(args: Array[String]) {
-    print("Hello basic-project!")
-  }
+    def main(args: Array[String]): Unit = {
+        val streamFactory = new TwitterStreamFactory()
+        val twitterStream = streamFactory.getInstance()
+        twitterStream.setOAuthConsumer("rwcbIdCTJZ91ROpA0yluLtCPB", "CZixZElCoi7QhJBhX9NJjIOFYWCtZHa9QCS3x3gDlafgZFl8XY")
+        val accessToken = new AccessToken("1664570156-zKRpnhJe8XN3iRSKs3FfIgVJ85EKZOGcbe82ci2", "t9t0AWDhiW3nmO3CcdLIrAheNOQmIGGqOrNO9buIhjxyh")
+        twitterStream.setOAuthAccessToken(accessToken)
+        twitterStream.addListener(new MyStatusListener())
+
+        twitterStream.sample()
+    }
+}
+
+class MyStatusListener extends StatusListener {
+    override def onStatus(status: Status): Unit = {
+        var lat: Double = 0
+        var lng: Double = 0
+        var urls: ArrayBuffer[String] = ArrayBuffer()
+        var medias: ArrayBuffer[String] = ArrayBuffer()
+
+        //. 位置情報が含まれていれば取得する
+        val location: GeoLocation = status.getGeoLocation()
+        if (location != null) {
+            val dlat = location.getLatitude()
+            val dlng = location.getLongitude()
+            lat = dlat
+            lng = dlng
+        }
+        var id = status.getId()
+        var text = status.getText()
+        var userid = status.getUser().getId()
+        var username = status.getUser().getScreenName()
+
+        var created = status.getCreatedAt()
+
+        //. ツイート本文にリンクURLが含まれていれば取り出す
+        var uentitys = status.getURLEntities()
+        if (uentitys != null && uentitys.length > 0) {
+            for (i <- 0 to uentitys.length) {
+                var uentity = uentitys(i)
+                var expandedURL = uentity.getExpandedURL()
+                urls += expandedURL
+            }
+        }
+
+        //. ツイート本文に画像／動画URLが含まれていれば取り出す
+        var mentitys = status.getMediaEntities()
+        if (mentitys != null && mentitys.length > 0) {
+            var list: Array[String] = null
+            for (i <- 0 to mentitys.length) {
+                var mentity = mentitys(i)
+                var expandedURL = mentity.getExpandedURL()
+                medias += expandedURL
+            }
+        }
+
+        //. 取り出した情報を表示する（以下では id, username, text のみ）
+        println("id = " + id + ", username =" + username + "text =" + text)
+    }
+
+    override def onDeletionNotice(statusDeletionNotice: StatusDeletionNotice): Unit = {}
+
+    override def onStallWarning(warning: StallWarning): Unit = {}
+
+    override def onScrubGeo(userId: Long, upToStatusId: Long): Unit = {}
+
+    override def onTrackLimitationNotice(numberOfLimitedStatuses: Int): Unit = {}
+
+    override def onException(ex: Exception): Unit = {}
 }
