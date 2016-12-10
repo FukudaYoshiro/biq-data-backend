e1f9aee918e912404d886b41a48e909f1d4745a5
FukudaYoshiro <elsa55queen@gmail.com>
2016-12-10 23:07:12 +0900

    success stream

--- build.sbt
+++ build.sbt
@@ -9,12 +9,9 @@ scalaVersion := "2.11.2"
 crossScalaVersions := Seq("2.10.4", "2.11.2")
 
 libraryDependencies ++= Seq(
-    "org.twitter4j" % "twitter4j-core" % "4.0.4",
-    "org.twitter4j" % "twitter4j-async" % "4.0.4",
-    "org.twitter4j" % "twitter4j-stream" % "4.0.4",
-    "org.twitter4j" % "twitter4j-media-support" % "4.0.4",
-    "org.scalatest" %% "scalatest" % "2.2.1" % "test",
-    "org.scalacheck" %% "scalacheck" % "1.11.5" % "test"
+    "org.apache.spark" %% "spark-core" % "1.2.0",
+    "org.apache.spark" %% "spark-streaming" % "1.2.0",
+    "org.apache.spark" %% "spark-streaming-twitter" % "1.2.0"
 )
 
 initialCommands := "import main._"
--- src/main/scala/App.scala
+++ src/main/scala/App.scala
@@ -1,76 +1,57 @@
 package main
 
-import twitter4j.auth.AccessToken
-import twitter4j.{GeoLocation, Status, StatusListener, _}
-import scala.collection.mutable.ArrayBuffer
+import org.apache.spark.streaming.{Seconds, StreamingContext}
+import org.apache.spark.SparkContext._
+import org.apache.spark.streaming.twitter._
+import org.apache.spark.SparkConf
+import org.apache.spark.streaming.StreamingContext._
+import org.apache.log4j.{Level, Logger}
 
 object App {
     def main(args: Array[String]): Unit = {
-        val streamFactory = new TwitterStreamFactory()
-        val twitterStream = streamFactory.getInstance()
-        twitterStream.setOAuthConsumer("rwcbIdCTJZ91ROpA0yluLtCPB", "CZixZElCoi7QhJBhX9NJjIOFYWCtZHa9QCS3x3gDlafgZFl8XY")
-        val accessToken = new AccessToken("1664570156-zKRpnhJe8XN3iRSKs3FfIgVJ85EKZOGcbe82ci2", "t9t0AWDhiW3nmO3CcdLIrAheNOQmIGGqOrNO9buIhjxyh")
-        twitterStream.setOAuthAccessToken(accessToken)
-        twitterStream.addListener(new MyStatusListener())
+        Logger.getLogger("org").setLevel(Level.OFF)
 
-        twitterStream.sample("ja")
+        // Set Twitter Access Keys
+        val config = new java.util.Properties
+        config.load(this.getClass().getClassLoader().getResourceAsStream("config.properties"))
+        System.setProperty("twitter4j.oauth.consumerKey", config.get("twitter_consumerKey").toString)
+        System.setProperty("twitter4j.oauth.consumerSecret", config.get("twitter_consumerSecret").toString)
+        System.setProperty("twitter4j.oauth.accessToken", config.get("twitter_accessToken").toString)
+        System.setProperty("twitter4j.oauth.accessTokenSecret", config.get("twitter_accessTokenSecret").toString)
+
+        // Create Stream
+        val filters = Array("Coffee", "Tea", "Alcohol")
+        val sparkConf = new SparkConf().setAppName("TwitterPopularTags").setMaster("local[*]")
+        val ssc = new StreamingContext(sparkConf, Seconds(2))
+        val stream = TwitterUtils.createStream(ssc, None, filters)
+
+        // Get RDD that has hashtags
+        val hashTags = stream.flatMap(status => status.getText.split(" ").filter(_.startsWith("#")))
+
+        // Get DStream
+        val topCounts60 = hashTags.map((_, 1)).reduceByKeyAndWindow(_ + _, Seconds(60))
+            .map{case (topic, count) => (count, topic)}
+            .transform(_.sortByKey(false))
+
+        val topCounts10 = hashTags.map((_, 1)).reduceByKeyAndWindow(_ + _, Seconds(10))
+            .map{case (topic, count) => (count, topic)}
+            .transform(_.sortByKey(false))
+
+        // Print popular hashtags
+        topCounts60.foreachRDD(rdd => {
+            val topList = rdd.take(10)
+            println("\nPopular topics in last 60 seconds (%s total):".format(rdd.count()))
+            topList.foreach{case (count, tag) => println("%s (%s tweets)".format(tag, count))}
+        })
+
+        topCounts10.foreachRDD(rdd => {
+            val topList = rdd.take(10)
+            println("\nPopular topics in last 10 seconds (%s total):".format(rdd.count()))
+            topList.foreach{case (count, tag) => println("%s (%s tweets)".format(tag, count))}
+        })
+
+        println("\n====================== Start. ======================")
+        ssc.start()
+        ssc.awaitTermination()
     }
-}
-
-class MyStatusListener extends StatusListener {
-    override def onStatus(status: Status): Unit = {
-        var lat: Double = 0
-        var lng: Double = 0
-        var urls: ArrayBuffer[String] = ArrayBuffer()
-        var medias: ArrayBuffer[String] = ArrayBuffer()
-
-        //. 位置情報が含まれていれば取得する
-        val location: GeoLocation = status.getGeoLocation()
-        if (location != null) {
-            val dlat = location.getLatitude()
-            val dlng = location.getLongitude()
-            lat = dlat
-            lng = dlng
-        }
-        var id = status.getId()
-        var text = status.getText()
-        var userid = status.getUser().getId()
-        var username = status.getUser().getScreenName()
-
-        var created = status.getCreatedAt()
-
-        //. ツイート本文にリンクURLが含まれていれば取り出す
-        var uentitys = status.getURLEntities()
-        if (uentitys != null && uentitys.length > 0) {
-            for (i <- 0 to uentitys.length) {
-                var uentity = uentitys(i)
-                var expandedURL = uentity.getExpandedURL()
-                urls += expandedURL
-            }
-        }
-
-        //. ツイート本文に画像／動画URLが含まれていれば取り出す
-        var mentitys = status.getMediaEntities()
-        if (mentitys != null && mentitys.length > 0) {
-            var list: Array[String] = null
-            for (i <- 0 to mentitys.length) {
-                var mentity = mentitys(i)
-                var expandedURL = mentity.getExpandedURL()
-                medias += expandedURL
-            }
-        }
-
-        //. 取り出した情報を表示する（以下では id, username, text のみ）
-        println("id = " + id + ", username =" + username + "text =" + text)
-    }
-
-    override def onDeletionNotice(statusDeletionNotice: StatusDeletionNotice): Unit = {}
-
-    override def onStallWarning(warning: StallWarning): Unit = {}
-
-    override def onScrubGeo(userId: Long, upToStatusId: Long): Unit = {}
-
-    override def onTrackLimitationNotice(numberOfLimitedStatuses: Int): Unit = {}
-
-    override def onException(ex: Exception): Unit = {}
 }
