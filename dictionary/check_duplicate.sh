40fa4140abb4d6431d93afb2c4124e2cda5ed8cb
FukudaYoshiro <elsa55queen@gmail.com>
2016-12-26 21:09:48 +0900

    [check_duplicate.sh] update display

--- dictionary/check_duplicate.sh
+++ dictionary/check_duplicate.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 PROJECT_DIR=$(cd $(dirname $0)/..; pwd)
-cat ${PROJECT_DIR}/dictionary/stopwords.csv | sort | uniq -c | awk '{print $1}' | grep -v 1
+cat ${PROJECT_DIR}/dictionary/stopwords.csv | sort | uniq -c | awk '{print $1 " " $2}' | grep -v 1
